c53ddc3c400f9179cf57e8431f1256457b852fc6
_Reasonable_Psycho_ <redkcz@gmail.com>
2025-07-28 01:48:21 +0200

    Fixed CMakePresets.json and added files for ECS from prevoius project (Commented that subdriectory in cmake for now )

---
+++ src/engine/tests/EngineTest.cpp
@@ -0,0 +1,11 @@
+#include <boost/test/unit_test.hpp>
+#include "Model.h"
+#include "AssetManager.hpp"
+
+BOOST_AUTO_TEST_SUITE(EngineTests)
+
+    BOOST_AUTO_TEST_CASE(Example) {
+        BOOST_TEST(true);
+    }
+
+BOOST_AUTO_TEST_SUITE_END()
---
+++ src/engine/tests/TestMain.cpp
@@ -0,0 +1,2 @@
+#define BOOST_TEST_MODULE Engine Test Suite
+#include <boost/test/unit_test.hpp>
